b7114f1224f7510f7ac40e70749b642e51cea516
ArtJDev <98458226+ArtJDev@users.noreply.github.com>
2022-09-20 13:54:50 +0300

    Update Customers.sql

--- Customers.sql
+++ Customers.sql
@@ -1,7 +1,7 @@
 create table customers (
 id serial primary key, 
-name VARCHAR(20),
-surname VARCHAR(30),
+name varchar(20),
+surname varchar(30),
 age smallint,
-phone_number VARCHAR(16)
+phone_number varchar(16)
 )
